10740f9c0f4a05c06e0b7fa4531df52d2b79d203
Angel Davchev <angeldavchev@gmail.com>
2016-11-12 13:54:47 +0100

    -Added responsive folder with laptop, tablet and mobile files -Added Raleway fonts folder in /tools -Added fonts file for pulling the fonts -Minor fixes

--- tools/js/main.js
+++ tools/js/main.js
@@ -1,5 +1,15 @@
 $(document).ready(function(){
 
 	// Your JavaScript goes here
-	
+	$(".ks-switch-unit").click(function(){
+		if($("#ks-switch-off").is(":checked")) {
+			$('#landing').addClass("landing-change");
+			$('#light').addClass('light-show');
+		} 
+		else {
+			$('#landing').removeClass("landing-change");
+			$('#light').removeClass('light-show');
+		}
+	});
+
 });
